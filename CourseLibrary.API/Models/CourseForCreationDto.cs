e6bf73507142d6f89d33d38e6b59117b45f83750
James Hart <james.elliot.hart@gmail.com>
2020-04-02 04:46:36 -0500

    data annotations for basic input validation

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
