041af096d1b37fdc3dac9099c124a822c3a21002
James Hart <james.elliot.hart@gmail.com>
2020-04-02 18:40:27 -0500

    ability to upsert course via patch

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -121,7 +121,20 @@ namespace CourseLibrary.API.Controllers
 
             var courseForAuthorFromRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
             if (courseForAuthorFromRepo == null)
-                return NotFound();
+            {
+                var courseDto = new CourseForUpdateDto();
+                patchDocument.ApplyTo(courseDto);
+                var courseToAdd = _mapper.Map<Entities.Course>(courseDto);
+                courseToAdd.Id = courseId;
+
+                _courseLibraryRepository.AddCourse(authorId, courseToAdd);
+                _courseLibraryRepository.Save();
+
+                var courseToReturn = _mapper.Map<CourseDto>(courseToAdd);
+                return CreatedAtAction(nameof(GetCourseForAuthor),
+                    new { authorId, courseId = courseToReturn.Id },
+                    courseToReturn);
+            }
             
             var courseToPatch = _mapper.Map<CourseForUpdateDto>(courseForAuthorFromRepo);
 
