bb4a49e687d56a145abf3df76400da7f42af4ff8
James Hart <james.elliot.hart@gmail.com>
2020-04-01 21:25:48 -0500

    ability to get multiple authors

--- CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +23,19 @@ namespace CourseLibrary.API.Controllers
             this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        [HttpGet("({ids})")]
+        public IActionResult GetAuthorCollection(
+            [FromRoute] [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null) return BadRequest();
+
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+            if (ids.Count() != authorEntities.Count()) return NotFound();
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            return Ok(authorsToReturn);
+        }
+
         [HttpPost]
         public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(
             IEnumerable<AuthorForCreationDto> authorCollection)
@@ -33,7 +48,12 @@ namespace CourseLibrary.API.Controllers
 
             _courseLibraryRepository.Save();
 
-            return Ok();
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+            return CreatedAtAction(nameof(GetAuthorCollection),
+                new { ids = idsAsString },
+                authorCollectionToReturn);
+
         }
     }
 }
